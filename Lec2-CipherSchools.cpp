579e27d2fd54804cd00e419948c56a6fab0cfe07
VIPUL SINGH <120162007+VIPULSINGH18@users.noreply.github.com>
2024-06-10 13:49:05 +0530

    Add files via upload

---
+++ Lec2-CipherSchools.cpp
@@ -0,0 +1,7 @@
+#include<iostream>    //Header for input and output function
+using namespace std;  // std make sure that cout function is a part iostream
+int main()            // This is main function
+{
+	cout<<"Hello World!!";   // cout function is used to dispaly the output
+	return 0;
+}
